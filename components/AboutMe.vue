81441fea26f9cb1d3236bcae22e8cf7b479ae622
snepsnepy <snepysneptest@gmail.com>
2024-11-24 16:40:45 +0200

    Added CV and fireworks logic.

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -61,13 +61,61 @@
           <p class="text-base lg:text-xl text-neutral font-bold font-tomorrow">
             MY RESUME
           </p>
-          <Icon
-            name="line-md:download-loop"
-            size="32"
-            class="text-neutral hover:text-accent hover:cursor-pointer"
-          />
+
+          <a href="/files/cv.pdf" download @click="getCV">
+            <Icon
+              name="line-md:download-loop"
+              size="32"
+              class="text-neutral hover:text-accent hover:cursor-pointer"
+            />
+          </a>
         </div>
       </BoxReveal>
     </GlowBorder>
   </div>
 </template>
+
+<script setup lang="ts">
+import confetti from "canvas-confetti";
+
+const getCV = () => {
+  launchFireworks();
+};
+
+// Function to trigger the confetti fireworks
+const launchFireworks = () => {
+  const duration = 5 * 1000; // 5 seconds
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  // Helper function to get a random value between a range
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = window.setInterval(() => {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      clearInterval(interval);
+      return;
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+
+    // Confetti from left side
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    });
+
+    // Confetti from right side
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    });
+  }, 250);
+};
+</script>
